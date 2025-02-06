de4ae30d3fac9cc4f0411cc4f8a8bb6fd060edd6
Evgeeso <evhenii.soroka@gmail.com>
2025-02-06 12:50:50 +0200

    FindUniqueElement

---
+++ Data_Structure/Random/Random(14)_findUniqueElement(arr) copy.js
@@ -0,0 +1,28 @@
+//You are given an array of integers where each element appears twice,
+//  except for one unique element that appears only once.
+//Implement a function in JavaScript to find the unique element.
+
+function findUniqueElement(arr) {
+    if(!Array.isArray(arr) )  {
+        console.log(`Function expecting array data type`);
+    } else if (arr.length === 0) {
+        console.log(`Array is empty`);
+    }
+
+    let countMap = new Map();
+
+    for (let num of arr ) {
+        countMap.set(num , (countMap.get(num) || 0 ) + 1)
+    }
+
+    for (let [key, value] of countMap) {
+        if ( value === 1) { 
+            return key
+        }
+    }
+}
+
+
+
+console.log(findUniqueElement([4, 3, 2, 3, 4])); // Output: 2
+console.log(findUniqueElement([1, 1, 2, 2, 3])); // Output: 3
